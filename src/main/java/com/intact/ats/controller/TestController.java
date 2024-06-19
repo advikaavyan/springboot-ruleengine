a95f92a4ec55ae311bba8c1b7b83dd7e79a2479d
Advika Avyan <avyanadvika@gmail.com>
2024-06-19 23:40:59 +0530

    initial

---
+++ src/main/java/com/intact/ats/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.intact.ats.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class TestController {
+    @Autowired
+    private ServletWebServerApplicationContext webServerAppCtxt;
+    int counter = 0;
+
+    @GetMapping("/test")
+    public Map<String, Object> executePost(HttpServletRequest request) {
+        log.info("test CALL CAME = ==  " + new Date() + ".");
+        Map<String, Object> result = new HashMap<>();
+
+
+        result.put("Msg:", "Hello App " + ++counter);
+        result.put("Date:", new Date());
+        result.put("URL:", request.getRequestURL());
+        result.put("getRemoteUser:", request.getRemoteUser());
+        result.put("IP:", request.getRemoteAddr());
+        result.put("Port:", webServerAppCtxt.getWebServer().getPort());
+        return result;
+
+
+    }
+
+}
