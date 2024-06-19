03466e462c4013d1c7a6d35f8d4f44d51bb6d054
Advika Avyan <avyanadvika@gmail.com>
2024-06-19 23:51:10 +0530

    spring boot upgrade

--- src/main/java/com/intact/ats/AtsService.java
+++ src/main/java/com/intact/ats/AtsService.java
@@ -2,10 +2,10 @@ package com.intact.ats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+        DataSourceAutoConfiguration.class}
 )
 
 public class AtsService {
